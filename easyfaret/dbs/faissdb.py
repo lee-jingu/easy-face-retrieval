2de355c10e066740ecbe302f8690496e81b69062
jglee <jingu.lee@actionpower.kr>
2023-08-20 15:16:04 +0900

    [UPDATE] initial commit

--- easyfaret/dbs/faissdb.py
+++ easyfaret/dbs/faissdb.py
@@ -1,6 +1,27 @@
+from __future__ import annotations
 import faiss
+import numpy as np
 
 
 class FaissDB:
-    def __init__(self):
-        pass
+    def __init__(self, name: str | None = None):
+        name = name if name is not None else "test"
+        self._collection = faiss.IndexFlatL2(512)
+        self._metadatas = []
+        self._name = name
+
+    def insert(self, embeddings: list[np.ndarray], metadatas: list[dict]) -> None:
+        assert len(embeddings) == len(
+            metadatas
+        ), "embeddings, metadatas must have the same length"
+
+        # for embedding, metadata in zip(embeddings, metadatas):
+        self._collection.add(np.array(embeddings))
+        self._metadatas = metadatas
+
+    def search(self, embedding: np.ndarray, n_results: int = 3):
+        embedding = np.array([embedding])
+        _, idxs = self._collection.search(embedding, n_results)
+        results = [self._metadatas[i] for i in idxs[0]]
+
+        return results
--- easyfaret/representation/farl.py
+++ easyfaret/representation/farl.py
@@ -2,56 +2,83 @@ import os
 
 import torch
 import clip
+import wget
 from PIL import Image
 from easyfaret import CKPT_PATH
+
 
 class FaRL:
     def __init__(self):
         path = self.check_and_download()
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        model, preprocess = clip.load("ViT-B/16", device="cpu")
+        print("clip loading...")
+        model, preprocess = clip.load(
+            "ViT-B/16", device="cpu", download_root=CKPT_PATH / "clip"
+        )
+        print("clip loaded")
         model = model.to(device)
         farl_ckpt = torch.load(path)
-        model.load_state_dict(farl_ckpt['state_dict'], strict=False)
+        model.load_state_dict(farl_ckpt["state_dict"], strict=False)
         model.eval()
 
         self.device = device
         self.model = model
         self.preprocess = preprocess
 
+        # TODO: Config file로 관리
         self.tag_names = dict(
-            gender = ['male', 'female'],
-            hair_color = ['black hair', 'blond hair', 'brown hair', 'gray hair'],
-            skin_color = ['white skin', 'black skin', 'yellow skin'],
-            age = ['old', 'young', 'child'],
-            glasses = ['with glasses', 'without glasses'],
-            costume = ['suit', 'shirt', 't-shirt', 'sweater', 'hoodie', 'jacket', 'dress', 'coat', 'jeans', 'pants', 'shorts', 'skirt'],
-            emotion = ['happy', 'sad', 'angry', 'surprised', 'disgusted', 'scared', 'neutral'],
+            gender=["male", "female"],
+            hair_color=[
+                "black hair",
+                "blond hair",
+                "brown hair",
+                "gray hair",
+                "white hair",
+            ],
+            emotion=[
+                "happy",
+                "sad",
+                "angry",
+                "surprised",
+                "disgusted",
+                "scared",
+                "neutral",
+            ],
+        )
+        self.tag_names_ko = dict(
+            gender=["남자", "여자"],
+            hair_color=["검정 머리", "금발", "갈색 머리", "회색머리", "흰머리"],
+            emotion=["행복", "슬픔", "분노", "놀람", "역겨움", "두려움", "중립"],
         )
 
-        self.tag_features = {k: model.encode_text(clip.tokenize(v).to(device)) for k, v in self.tag_names.items()}
-
+        tag_features = {}
+        with torch.no_grad():
+            for k, v in self.tag_names.items():
+                tag_features[k] = clip.tokenize(v).to(self.device)
+        self.tag_features = tag_features
 
     def check_and_download(self):
-        path = CKPT_PATH / 'farl' / 'FaRL-Base-Patch16-LAIONFace20M-ep16.pth'
+        path = CKPT_PATH / "farl" / "FaRL-Base-Patch16-LAIONFace20M-ep16.pth"
+        print("얼굴 임베딩 모델 loading...")
         if not path.exists():
             os.makedirs(path.parent, exist_ok=True)
-            url = 'https://github.com/FacePerceiver/FaRL/releases/download/pretrained_weights/FaRL-Base-Patch16-LAIONFace20M-ep16.pth'
-            os.system(f'wget {url} -P {path}')
+            url = "https://github.com/FacePerceiver/FaRL/releases/download/pretrained_weights/FaRL-Base-Patch16-LAIONFace20M-ep16.pth"
+            wget.download(url, str(path))
+        print("얼굴 임베딩 모델 loaded")
         return path
-    
+
     def get_image_embedding(self, image: Image.Image):
         image = self.preprocess(image).unsqueeze(0).to(self.device)
         with torch.no_grad():
             image_features = self.model.encode_image(image)
         return image_features.detach().cpu().numpy().squeeze()
-    
+
     def get_tags(self, image: Image.Image):
         image = self.preprocess(image).unsqueeze(0).to(self.device)
 
         model = self.model
         tag_features = self.tag_features
-        tag_names = self.tag_names
+        tag_names = self.tag_names_ko
 
         tags = {}
         with torch.no_grad():
@@ -59,12 +86,5 @@ class FaRL:
                 logit_per_image, _ = model(image, t_feature)
                 max_idx = torch.argmax(logit_per_image, dim=-1)
                 tags[k] = tag_names[k][max_idx]
-        
-        return tags
 
-    def __call__(self, image: Image.Image) -> dict:
-        ret = {
-            'embedding': self.get_image_embedding(image),
-            'tags': self.get_tags(image),
-        }
-        return ret
+        return tags
